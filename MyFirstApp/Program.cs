18376c45ff7a6f0e4e76a3bbe5e72e366173290e
Rasmus <rasmus.hirvesoo@tktk.ee>
2023-01-25 15:00:54 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,13 @@
+﻿//This is a comment to my code
+
+//rakendus küsib kasutajalt, et ta sisestaks oma nime
+//rakendus tervitab kasutajad nimepidi
+
+Console.WriteLine("Enter your name:"); //Output
+//string - sõna
+string userName = Console.ReadLine(); //Input
+
+Console.WriteLine("Hello"+ ", " + userName + "!"); //Output
+//string interpolation
+
+Console.WriteLine($"Hello, {userName}!"); //Output
